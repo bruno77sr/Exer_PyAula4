e73e25e2a4f5c26a51e7959b10911e4e4878e1bf
Logon Anália Franco <bruno77sr@gmail.com>
2022-03-30 19:20:31 -0300

    exemplo 2

--- exercicio5.py
+++ exercicio5.py
@@ -1,2 +1,8 @@
+import math
+
+
 graus =  int(input("Digite um valor em Graus:"))
 
+rad =  graus / 180
+
+print(" O seno é: %.2f  \nCosseno é: %.2f \nTangente é: %.2f" %(math.sin(rad), math.cos(rad), math.tan(rad)))
