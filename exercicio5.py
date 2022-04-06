1e98f8b08b16a9dd378493bab3318147df57137e
Logon Anália Franco <bruno77sr@gmail.com>
2022-04-06 18:55:32 -0300

    ex5 att

--- exercicio5.py
+++ exercicio5.py
@@ -2,6 +2,7 @@ import math
 
 graus =  int(input("Digite um valor em Graus:"))
 
-rad =  graus / 180
+rad =  math.radians(graus)
 
-print(" O seno é: %.2f  \nCosseno é: %.2f \nTangente é: %.2f" %(math.sin(rad), math.cos(rad), math.tan(rad)))
+print("O radiano é %.3f " %(rad))
+print(" O seno é: %.3f  \nCosseno é: %.3f \nTangente é: %.3f" %(math.sin(rad), math.cos(rad), math.tan(rad)))
